6109c48c96a97147bac849fb111886b46b56c7e0
javtau <javier.lozano.almeda@gmail.com>
2017-02-10 18:56:28 +0100

    Merge branch 'victor' of F:\Dropbox\modulo\SEGUNDO\WORKSPACE_ANDROIDSTUDIO\MocMocApp with conflicts.

--- app/src/main/java/com/jjv/proyectointegradorv1/Objects/Publicacion.java
+++ app/src/main/java/com/jjv/proyectointegradorv1/Objects/Publicacion.java
@@ -34,6 +34,8 @@ public class Publicacion implements Parcelable{
         this.precio = precio;
     }
 
+
+
     public Map<String, Object> toMap() { //creamos una lista con cada uno de los atibustos del objeto
         HashMap<String, Object> result = new HashMap<>();
         result.put("usuario", usuario);
@@ -130,4 +132,16 @@ public class Publicacion implements Parcelable{
 
 
     }
+
+    public static final Creator<Publicacion> CREATOR = new Creator<Publicacion>() {
+        @Override
+        public Publicacion createFromParcel(Parcel in) {
+            return new Publicacion(in);
+        }
+
+        @Override
+        public Publicacion[] newArray(int size) {
+            return new Publicacion[size];
+        }
+    };
 }
