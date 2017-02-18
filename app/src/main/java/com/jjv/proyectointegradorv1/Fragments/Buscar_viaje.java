96e724cdb4181f93bf6907fd8d35f3873b617600
MunozVictor <viketore18@hotmail.com>
2017-02-18 12:48:27 +0100

    card y listener del recycler view implementados

--- app/src/main/java/com/jjv/proyectointegradorv1/Fragments/Buscar_viaje.java
+++ app/src/main/java/com/jjv/proyectointegradorv1/Fragments/Buscar_viaje.java
@@ -12,6 +12,8 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
@@ -29,6 +31,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.jjv.proyectointegradorv1.Adapters.DialogReserva;
 import com.jjv.proyectointegradorv1.Adapters.Publicaciones_Adapter;
+import com.jjv.proyectointegradorv1.Adapters.Publicaciones_RV_adapter;
 import com.jjv.proyectointegradorv1.Objects.Publicacion;
 import com.jjv.proyectointegradorv1.R;
 
@@ -38,7 +41,7 @@ import java.util.ArrayList;
  * Created by javi0 on 11/01/2017.
  */
 
-public class Buscar_viaje extends Fragment implements AdapterView.OnItemClickListener {
+public class Buscar_viaje extends Fragment  {
 
     private final String TAG = Buscar_viaje.class.getSimpleName();
     private ListView listaPublicaciones;
@@ -48,6 +51,8 @@ public class Buscar_viaje extends Fragment implements AdapterView.OnItemClickLis
     private ChildEventListener childEvent;
     private Publicacion publica;
     private Dialog customDialog ;
+    private RecyclerView rv;
+    private Publicaciones_RV_adapter.OnItemClickListener listenerRv;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_buscar_viaje, container, false);
@@ -57,28 +62,46 @@ public class Buscar_viaje extends Fragment implements AdapterView.OnItemClickLis
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        listaPublicaciones = (ListView) view.findViewById(R.id.lista_publicaciones);
-        listaPublicaciones.setOnItemClickListener(this);
+        // listaPublicaciones = (ListView) view.findViewById(R.id.lista_publicaciones);
+        //listaPublicaciones.setOnItemClickListener(this);
+        // implementacion recycler
+        rv= (RecyclerView) view.findViewById(R.id.lista_publicaciones);
+        /*
+        Si estás seguro que el tamaño del RecyclerView no se cambiará,
+        puedes añadirlo lo siguiente para mejorar el desempeño:
+         */
+        rv.setHasFixedSize(true);
+        //para manejar el recycler view como una lista es necesario un manager
+        LinearLayoutManager llm = new LinearLayoutManager(getContext());
+        rv.setLayoutManager(llm);
+
+
 
         myRef = database.getReference("trip");// hacemos referencia a la rama donde se almacenan todos los viajes
 
         childEvent = new ChildEventListener() {
-            Publicaciones_Adapter adapt;
+            //Publicaciones_Adapter adapt;
+            Publicaciones_RV_adapter adapt;
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
                 publica = dataSnapshot.getValue(Publicacion.class);
                 publicaciones.add(publica);
-                adapt=  new Publicaciones_Adapter(getContext(),publicaciones);
-                listaPublicaciones.setAdapter(adapt);
+                //  adapt=  new Publicaciones_Adapter(getContext(),publicaciones);
+                adapt = new Publicaciones_RV_adapter(publicaciones,listenerRv);
+                //listaPublicaciones.setAdapter(adapt);
+                rv.setAdapter(adapt);
                 Log.d(TAG, publica.getOrigen());
+
             }
 
             @Override
             public void onChildChanged(DataSnapshot dataSnapshot, String s) {
                 publica = dataSnapshot.getValue(Publicacion.class);
                 publicaciones.add(publica);
-                adapt=  new Publicaciones_Adapter(getContext(),publicaciones);
-                listaPublicaciones.setAdapter(adapt);
+                //adapt=  new Publicaciones_Adapter(getContext(),publicaciones);
+                adapt = new Publicaciones_RV_adapter(publicaciones,listenerRv);
+                rv.setAdapter(adapt);
+                //listaPublicaciones.setAdapter(adapt);
             }
 
             @Override
@@ -104,21 +127,29 @@ public class Buscar_viaje extends Fragment implements AdapterView.OnItemClickLis
             }
         };
         myRef.addChildEventListener(childEvent);
+        listenerRv=initListener();
 
-
-        Publicaciones_Adapter adapter = new Publicaciones_Adapter(getContext(),publicaciones);
-        listaPublicaciones.setAdapter(adapter);
-
-    }
-
-    @Override
-    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-        showDialog(i);
+        Publicaciones_RV_adapter adapter = new Publicaciones_RV_adapter(publicaciones,listenerRv);
+        rv.setAdapter(adapter);
+        // Publicaciones_Adapter adapter = new Publicaciones_Adapter(getContext(),publicaciones);
+        // listaPublicaciones.setAdapter(adapter);
 
 
     }
 
-    private void showDialog(int pos){
+    private Publicaciones_RV_adapter.OnItemClickListener initListener() {
+
+        Publicaciones_RV_adapter.OnItemClickListener l= new Publicaciones_RV_adapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(Publicacion item) {
+                showDialog(item);
+            }
+
+
+    };
+        return l;}
+
+    private void showDialog(Publicacion p){
 
 
         customDialog =  new Dialog(getContext(),R.style.Theme_Dialog_Translucent);
@@ -140,6 +171,8 @@ public class Buscar_viaje extends Fragment implements AdapterView.OnItemClickLis
                 return false;
             }
         });
+
+
 
 
         customDialog.show();
