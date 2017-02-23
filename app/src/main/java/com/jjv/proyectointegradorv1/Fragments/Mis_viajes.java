b6875557c6f81c907f4dbb0ada76e1ce689d4dc0
MunozVictor <viketore18@hotmail.com>
2017-02-23 21:26:50 +0100

    init borrar publicacion por conductor , corregido texto login y registro

--- app/src/main/java/com/jjv/proyectointegradorv1/Fragments/Mis_viajes.java
+++ app/src/main/java/com/jjv/proyectointegradorv1/Fragments/Mis_viajes.java
@@ -1,13 +1,18 @@
 package com.jjv.proyectointegradorv1.Fragments;
 
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -19,6 +24,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.jjv.proyectointegradorv1.Adapters.Publicaciones_Adapter;
+import com.jjv.proyectointegradorv1.Adapters.Publicaciones_RV_adapter;
 import com.jjv.proyectointegradorv1.Objects.Publicacion;
 import com.jjv.proyectointegradorv1.R;
 
@@ -28,7 +34,7 @@ import java.util.ArrayList;
  * Created by javi0 on 11/01/2017.
  */
 
-public class Mis_viajes extends Fragment {
+public class Mis_viajes extends Fragment  {
 
     private FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
     private ListView listaMisViajes;
@@ -38,6 +44,7 @@ public class Mis_viajes extends Fragment {
     private ChildEventListener childEvent;
     private Publicacion publicacion;
     private ArrayList<Publicacion> publicaciones;
+    private Dialog customDialog ;
 
 
 
@@ -94,8 +101,41 @@ public class Mis_viajes extends Fragment {
         dbref.addChildEventListener(childEvent);
         adapter = new Publicaciones_Adapter(view.getContext(),publicaciones);
         listaMisViajes.setAdapter(adapter);
+        listaMisViajes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                showDialog(publicaciones.get(position));
+            }
+        });
 
 
+
+    }
+
+    private void showDialog(Publicacion p){
+
+        customDialog =  new MisViajesDialog(getContext(), R.style.Theme_Dialog_Translucent, p);
+        //deshabilitamos el título por defecto
+        customDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //obligamos al usuario a pulsar los botones para cerrarlo
+        customDialog.setCancelable(false);
+        //establecemos el contenido de nuestro dialog
+        //LayoutInflater factory = LayoutInflater.from(getContext());
+        //View dView = factory.inflate(R.layout.dialog_fragment_reservar, null);
+        customDialog.setContentView(R.layout.dialog_fragment_misviajes);
+        customDialog.setOnKeyListener(new DialogInterface.OnKeyListener() {
+            @Override
+            public boolean onKey(DialogInterface dialogInterface, int i, KeyEvent keyEvent) {
+
+                if(i==KeyEvent.KEYCODE_BACK){
+                    customDialog.dismiss();
+                    return true;
+
+                }
+                return false;
+            }
+        });
+        customDialog.show();
     }
 
 }
